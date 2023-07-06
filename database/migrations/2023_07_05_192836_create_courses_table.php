dcccc3fd5e8597cf6819c4b9dde23c81de977002
daniel ngwasi <damnngwasi@gmail.com>
2023-07-06 08:01:35 +0300

    migrations

--- database/migrations/2023_07_05_192836_create_courses_table.php
+++ database/migrations/2023_07_05_192836_create_courses_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('courses', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string('course_id');
+            $table->string('name');
+            $table->foreignId('teacher_id')->constrained();
+            $table->string('credit');
+            $table->string('grade');
             $table->timestamps();
         });
     }
