1659707fa35cdd75204ba3f68f150353738e1d5a
kelvinmurimi <murimikelvin229@gmail.com>
2023-07-07 05:34:08 +0300

    minor change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,58 +1,51 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        <x-banner />
 
-    <!-- Styles -->
-    @livewireStyles
-</head>
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-menu')
 
-<body class="font-sans antialiased">
-    <x-banner />
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
 
+   
 
-    <!-- Page Content -->
-    <main>
-        {{ $slot }}
-    </main>
+            <!-- Page Content -->
+            <main>
+                @include('includes.messages')
+              
+                {{ $slot }} 
+            </main>
+        </div>
+        <div>
+            @include('layouts.footer')
+        </div>
+        @stack('modals')
 
-  <div class="min-h-screen bg-gray-100">
-        @livewire('navigation-menu')
-
-
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-        @if (Session::has('success'))
-            <p class="alert alert-success">{{ Session::get('success') }}</p>
-        @endif
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
-@include('layouts.footer')
-    @stack('modals')
-
-    @livewireScripts
-</body>
-
+        @livewireScripts
+    </body>
 </html>
